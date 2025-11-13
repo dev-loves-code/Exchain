471dc4057a7151362a3385658679e088afc1bf3c
Bahij Ghazal <bahijghazal1@gmail.com>
2025-11-13 13:37:13 +0200

    finished request refund controller and services

--- app/Http/Controllers/RefundRequestsController.php
+++ app/Http/Controllers/RefundRequestsController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\RefundRequest;
 use App\Models\Transaction;
 use App\Models\Wallet;
+use App\Services\RefundRequestService;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Nette\Schema\ValidationException;
 
 class RefundRequestsController extends Controller
 {
+    protected $refundRequestService;
+    public function __construct(RefundRequestService $refundRequestService){
+        $this->refundRequestService = $refundRequestService;
+    }
+
     public function create(Request $request)
     {
 
@@ -22,8 +29,8 @@ class RefundRequestsController extends Controller
             return response() -> json(['errors' => $e->errors()]);
         }
 
-        $already_exists = RefundRequest::where('transaction_id',$request->transaction_id);
-        if (!is_null($already_exists) ) {
+        $already_exists = RefundRequest::where('transaction_id',$request->transaction_id)->exists();
+        if ($already_exists) {
             return response() -> json(['errors' => ["Refund request for this transaction already exists!"]],409);
         }
 
@@ -47,11 +54,14 @@ class RefundRequestsController extends Controller
 
     public function viewSingleRefundRequest(Request $request,$id)
     {
-        $user_id = $request->user()->user_id;
-        $transaction = Transaction::find($id);
+        try {
 
-        $refund_request = RefundRequest::where('transaction_id',$id)->firstOrFail();
-        $wallet = Wallet::find($transaction->sender_wallet_id);
+        $user_id = $request->user()->user_id;
+
+        $refund_request = RefundRequest::findOrFail($id);
+        $transaction = Transaction::findOrFail($refund_request->transaction_id);
+
+        $wallet = Wallet::findOrFail($transaction->sender_wallet_id);
 
 
         if ($user_id !== $wallet->user_id && $request->user()->role->role_name !== "admin")
@@ -67,6 +77,10 @@ class RefundRequestsController extends Controller
             'description' => $refund_request -> description,
             'status' => $refund_request -> status,
         ]);
+        }
+        catch(Exception $e){
+            return response() -> json(['errors' => $e->getMessage()]);
+        }
     }
 
     public function viewAllRefundRequests(Request $request)
@@ -79,7 +93,7 @@ class RefundRequestsController extends Controller
                 'status' => ['nullable',Rule::in($valid_statuses)],
                 'order_by' => ['nullable',Rule::in($valid_order)],
             ]);
-        }catch(ValidationException $e){
+        }catch(Exception $e){
             return response() -> json(['errors' => $e->errors()],422);
         }
         $user_role = $request->user()->role->role_name;
@@ -124,5 +138,91 @@ class RefundRequestsController extends Controller
             })
         );
     }
+
+    public function approveRefund(Request $request, $id){
+         try{
+             if($request->user()->role->role_name !== "admin"){
+                 return response() -> json([
+                     'message' => 'You do not have permission to approve refund requests!',
+                 ],401);
+             }
+
+              $refundRequest = $this->refundRequestService->approveRefundRequest($id);
+
+             return response() -> json([
+                 'success' => true,
+                 'message' => 'Refund request approved',
+                 'data' => $refundRequest,
+             ],200);
+
+         }catch(Exception $e){
+             return response() -> json(['errors' => $e->getMessage()],400);
+         }
+    }
+
+    public function rejectRefund(Request $request,$id){
+        try{
+
+            if($request->user()->role->role_name !== "admin"){
+                return response() -> json([
+                    'message' => 'You do not have permission to approve refund requests!',
+                ],401);
+            }
+
+            $validated = $request->validate([
+                'rejected_reason' => 'nullable|string|max:255',
+            ]);
+
+            $refundRequest = $this->refundRequestService->rejectRefundRequest(
+                $id,
+                $validated['rejected_reason']??null
+            );
+
+            return response() -> json([
+                'success' => true,
+                'message' => 'Refund request rejected',
+                'data' => $refundRequest,
+            ],200);
+
+        }catch(Exception $e){
+            return response() -> json(['errors' => $e->getMessage()],400);
+        }
+    }
+
+    public function completeRefund(Request $request,$id){
+
+        try{
+            $user_id = $request->user()->user_id;
+
+            $refundRequest = $this->refundRequestService->completeRefundRequest($id,$user_id);
+
+            return response() -> json([
+                'success' => true,
+                'message' => 'Refund request completed successfully',
+                'data' => $refundRequest,
+            ],200);
+
+        }catch(Exception $e){
+            return response() -> json(['errors' => $e->getMessage()],400);
+        }
+    }
+
+    public function cancelRefund(Request $request,$id){
+        try{
+
+            $user_id = $request->user()->user_id;
+            $refund_request = $this->refundRequestService->cancelRefundRequest($id,$user_id);
+
+            return response() -> json([
+                'success' => true,
+                'message' => 'Refund request canceled successfully',
+            ],200);
+
+        }catch(Exception $e){
+            return response() -> json(['errors' => $e->getMessage()],400);
+        }
+    }
+
+
 }
 
---
+++ app/Services/RefundRequestService.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\RefundRequest;
+use App\Models\Transaction;
+use App\Models\Wallet;
+use Exception;
+use Nette\Schema\ValidationException;
+
+class RefundRequestService
+{
+        public function approveRefundRequest($refund_id){
+                $refund_request = RefundRequest::findOrFail($refund_id);
+
+
+                if ($refund_request->status !== 'pending') {
+                    throw new Exception('Only pending refund requests can be accepted');
+                }
+
+                $transaction = Transaction::findOrFail($refund_request -> transaction_id);
+
+                $refund_amount = $transaction -> transfer_amount;
+
+                $wallet = Wallet::findOrFail($transaction -> sender_wallet_id);
+
+                $wallet->balance += $refund_amount;
+                $wallet->save();
+
+                $refund_request -> status = 'approved';
+                $refund_request -> save();
+
+                return $refund_request;
+
+
+        }
+
+        public function rejectRefundRequest($refund_id, $reject_reason = null)
+        {
+                    $refund_request = RefundRequest::findOrFail($refund_id);
+
+                    if ($refund_request->status !== 'pending') {
+                        throw new Exception('Only pending refund requests can be rejected');
+                    }
+
+
+                    $refund_request -> status = 'rejected';
+                    $refund_request -> save();
+
+                    return $refund_request;
+
+        }
+
+        public function completeRefundRequest($refund_id,$user_id){
+
+                $refund_request = RefundRequest::findOrFail($refund_id);
+
+                $transaction = Transaction::findOrFail($refund_request -> transaction_id);
+
+                if($transaction -> sender_wallet_id !== $user_id){
+                    throw new Exception('You can only complete refund requests of your own');
+                }
+
+                if ($refund_request->status !== 'approved') {
+                    throw new Exception('Only approved refund requests can be completed');
+                }
+                $refund_request -> status = 'completed';
+                $refund_request -> save();
+
+                return $refund_request;
+
+
+
+        }
+
+        public function cancelRefundRequest($refund_id,$user_id){
+                $refund_request = RefundRequest::findOrFail($refund_id);
+
+                if($refund_request -> status !== 'pending'){
+                    throw new Exception('Only pending refund requests can be cancelled');
+                }
+
+                $transaction = Transaction::findOrFail($refund_request -> transaction_id);
+
+                if($transaction -> sender_wallet_id !== $user_id){
+                    throw new Exception('You can only cancel refund requests of your own');
+                }
+
+                $refund_request -> delete();
+
+                return true;
+        }
+
+}
